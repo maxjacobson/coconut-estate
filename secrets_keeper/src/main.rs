ca749c65877c14e53dd6f5187151c6f13f01bd7b
Max Jacobson <max@hardscrabble.net>
2018-05-21 23:13:01 -0400

    Actually write the secrets!

--- secrets_keeper/src/main.rs
+++ secrets_keeper/src/main.rs
@@ -6,11 +6,22 @@ use clap::{App as ClapApp, AppSettings, Arg, SubCommand};
 extern crate serde_derive;
 
 extern crate actix_web;
-use actix_web::{middleware, server, App, HttpResponse, Json, http::Method};
+use actix_web::{middleware, server, App, HttpRequest, HttpResponse, Json, http::Method};
 
 extern crate env_logger;
 #[macro_use]
 extern crate log;
+
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+use std::fs::OpenOptions;
+
+#[derive(Debug)]
+struct AppState {
+    location: String,
+}
 
 #[derive(Debug, Deserialize)]
 struct Secret {
@@ -18,8 +29,16 @@ struct Secret {
     value: String,
 }
 
-fn create_secret(secret: Json<Secret>) -> HttpResponse {
-    println!("secret: {:#?}", secret);
+fn create_secret(data: (HttpRequest<AppState>, Json<Secret>)) -> HttpResponse {
+    let (req, secret) = data;
+    let state: &AppState = req.state();
+
+    let path = Path::new(&state.location).join(&secret.name);
+    if !Path::exists(&path) {
+        File::create(&path).unwrap(); // TODO: add failure
+    }
+    let mut file = OpenOptions::new().write(true).open(&path).unwrap(); // TODO: add failure
+    file.write_all(secret.value.as_bytes()).unwrap(); // TODO: add failure
 
     HttpResponse::Ok().into()
 }
@@ -30,27 +49,41 @@ fn main() {
         .setting(AppSettings::SubcommandRequired)
         .version(crate_version!())
         .subcommand(
-            SubCommand::with_name("run").about("Run the server").arg(
-                Arg::with_name("binding")
-                    .short("b")
-                    .long("binding")
-                    .value_name("BINDING")
-                    .help("What to bind the service to (e.g. localhost:5001)")
-                    .required(true)
-                    .takes_value(true),
-            ),
+            SubCommand::with_name("run")
+                .about("Run the server")
+                .arg(
+                    Arg::with_name("binding")
+                        .short("b")
+                        .long("binding")
+                        .value_name("BINDING")
+                        .help("What to bind the service to (e.g. localhost:5001)")
+                        .required(true)
+                        .takes_value(true),
+                )
+                .arg(
+                    Arg::with_name("location")
+                        .short("l")
+                        .long("location")
+                        .value_name("LOCATION")
+                        .help("Path to a directory where secrets ought to be kept")
+                        .required(true)
+                        .takes_value(true),
+                ),
         )
         .get_matches();
 
     if let Some(matches) = matches.subcommand_matches("run") {
         let binding = matches.value_of("binding").unwrap().to_string();
+        let location = matches.value_of("location").unwrap().to_string();
+
         env_logger::init();
 
         info!("Starting...");
 
-        server::new(|| {
-            App::new()
-                .middleware(middleware::Logger::default())
+        server::new(move || {
+            App::with_state(AppState {
+                location: location.clone(),
+            }).middleware(middleware::Logger::default())
                 .resource("/secrets", |r| r.method(Method::POST).with(create_secret))
         }).bind(&binding)
             .expect(&format!("Can not bind to {}", binding))
