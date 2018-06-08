fe8fa4a3ef72bdfbcea0fd1c1fc6da5f40319e85
Max Jacobson <max@hardscrabble.net>
2018-06-08 01:09:14 -0400

    Add a production environment
    This is a thing now!
    
    Will probably make the provision script more smart to create that domain
    etc.

--- toolbox/src/environments.rs
+++ toolbox/src/environments.rs
@@ -1,6 +1,7 @@
 use failure::Error;
 
 pub static DEFAULT_ENVIRONMENT_NAME: &'static str = "development";
+static PRODUCTION: &'static str = "production";
 
 #[derive(Fail, Debug)]
 #[fail(display = "Unknown environment {}", name)]
@@ -11,12 +12,15 @@ struct UnknownEnvironment {
 #[derive(Debug)]
 pub enum Environment {
     Development,
+    Production,
 }
 
 impl Environment {
     pub fn from_name(name: &str) -> Result<Self, Error> {
         if name == DEFAULT_ENVIRONMENT_NAME {
             Ok(Environment::Development)
+        } else if name == PRODUCTION {
+            Ok(Environment::Production)
         } else {
             Err(UnknownEnvironment {
                 name: name.to_string(),
@@ -27,6 +31,7 @@ impl Environment {
     pub fn secrets_keeper_root(&self) -> String {
         match self {
             Environment::Development => "http://localhost:5001".to_string(),
+            Environment::Production => "https://secrets.ops.coconutestate.top".to_string(),
         }
     }
 }
