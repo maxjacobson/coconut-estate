9b60baf2995f344f35a5091e742f93346b29d716
Max Jacobson <max@hardscrabble.net>
2018-06-20 00:04:17 -0400

    Add a basic website binary crate
    It just renders a micro HTML page and loads a static asset.
    
    Later on, the actix-web service will have more routes that represent the
    API, and the JS will be a single-page-app.
    
    I think, anyway.
    
    This is just a stake in the ground to build on. There's gonna be a
    website as part of all this. There's a lot more to figure out, but that
    part feels figured out.

---
+++ website/src/main.rs
@@ -0,0 +1,126 @@
+#[macro_use]
+extern crate clap;
+use clap::{App as ClapApp, AppSettings, Arg, SubCommand};
+
+extern crate actix_web;
+use actix_web::http::{Method, StatusCode};
+use actix_web::{fs, middleware, server, App, HttpRequest, HttpResponse};
+
+extern crate env_logger;
+#[macro_use]
+extern crate log;
+
+extern crate openssl;
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+
+fn hello_world(_req: HttpRequest) -> HttpResponse {
+    HttpResponse::build(StatusCode::OK)
+        .content_type("text/html; charset=utf-8")
+        .body(include_str!("../root.html"))
+}
+
+fn main() {
+    let matches = ClapApp::new("Website")
+        .about("A user-facing website")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .setting(AppSettings::VersionlessSubcommands)
+        .version(crate_version!())
+        .subcommand(
+            SubCommand::with_name("run")
+                .about("Run the server")
+                .arg(
+                    Arg::with_name("binding")
+                        .short("b")
+                        .long("binding")
+                        .value_name("BINDING")
+                        .help("What to bind the service to (e.g. localhost:5001)")
+                        .required(true)
+                        .takes_value(true),
+                )
+                .arg(
+                    Arg::with_name("ssl")
+                        .long("ssl")
+                        .help("Pass this flag to opt in to serving the website over SSL")
+                        .required(false)
+                        .takes_value(false)
+                        .requires_all(&["private key file", "certificate chain file"]),
+                )
+                .arg(
+                    Arg::with_name("private key file")
+                        .short("k")
+                        .long("key")
+                        .value_name("PATH TO PRIVATE KEY FILE")
+                        .help("The path to the private key file")
+                        .required(false)
+                        .takes_value(true)
+                        .requires_all(&["ssl", "certificate chain file"]),
+                )
+                .arg(
+                    Arg::with_name("certificate chain file")
+                        .short("c")
+                        .long("cert")
+                        .value_name("PATH TO CERTIFICATE CHAIN FILE")
+                        .help("The path to the certificate chain file")
+                        .required(false)
+                        .takes_value(true)
+                        .requires_all(&["private key file", "ssl"]),
+                ),
+        )
+        .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("run") {
+        let binding = matches
+            .value_of("binding")
+            .expect("binding to be provided (it's required)")
+            .to_string();
+        let use_ssl = matches.is_present("ssl");
+
+        env_logger::init();
+
+        info!("Starting...");
+
+        let unbound_server = server::new(move || {
+            App::new()
+                .middleware(middleware::Logger::default())
+                .resource("/", |r| {
+                    r.method(Method::GET).with(hello_world);
+                })
+                .handler(
+                    "/assets/",
+                    fs::StaticFiles::new("./website/assets").index_file("index.html"),
+                )
+        });
+
+        if use_ssl {
+            let path_to_private_key_file = matches
+                .value_of("private key file")
+                .expect("private key file to be present (It's required when ssl is passed")
+                .to_string();
+
+            let path_to_certificate_chain_file = matches
+                .value_of("certificate chain file")
+                .expect("certificate chain file to be present (It's required when ssl is passed")
+                .to_string();
+
+            let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+            builder
+                .set_private_key_file(&path_to_private_key_file, SslFiletype::PEM)
+                .unwrap();
+            builder
+                .set_certificate_chain_file(&path_to_certificate_chain_file)
+                .unwrap();
+
+            unbound_server
+                .bind_ssl(&binding, builder)
+                .expect(&format!("Can not bind to {}", binding))
+                .run();
+        } else {
+            unbound_server
+                .bind(&binding)
+                .expect(&format!("Can not bind to {}", binding))
+                .run();
+        }
+    } else {
+        panic!("whaaaaaaat");
+    }
+}
