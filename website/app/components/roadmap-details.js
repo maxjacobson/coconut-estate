50eefdf4f35feee9c0eb88d7b2b32d9b7b0b3936
Max Jacobson <max@hardscrabble.net>
2018-08-10 22:06:59 -0400

    Add a destroy button for roadmaps (doesn't work)

--- website/app/components/roadmap-details.js
+++ website/app/components/roadmap-details.js
@@ -1,4 +1,11 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  actions: {
+    destroy(roadmap) {
+      if (confirm("Are you sure?")) {
+        alert(`I'd love to destroy ${roadmap.name}, but I don't know how`);
+      }
+    }
+  },
 });
